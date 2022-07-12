b69da02d32339d96b5b88501886f066a85536d45
AndreasSitumorang <andreassitumorang098@gmail.com>
2022-07-12 14:32:15 +0700

    begin order food chart

--- src/App.js
+++ src/App.js
@@ -1,41 +1,8 @@
-import React, { useEffect, useState } from 'react';
-
-import Login from './components/Login/Login';
-import Home from './components/Home/Home';
-import MainHeader from './components/MainHeader/MainHeader';
-import contextAuth from './Context/auth-contrext';
-
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  useEffect( () => {
-    const storedLocalUser = localStorage.getItem('isLogined');
-    if(storedLocalUser === '1'){
-      setIsLoggedIn(true);
-    }
-  },[]);
-
-  const loginHandler = (email, password) => {
-
-    localStorage.setItem('isLogined', '1');
-    setIsLoggedIn(true);
-  };
-
-  const logoutHandler = () => {
-    localStorage.removeItem('isLogined')
-    setIsLoggedIn(false);
-  };
-
   return (
-    <contextAuth.Provider value={{
-      isLoggedIn : isLoggedIn,
-      onLogout : logoutHandler,
-  }}>
-      <MainHeader/>
-      <main>
-        {!isLoggedIn && <Login onLogin={loginHandler} />}
-        {isLoggedIn && <Home onLogout={logoutHandler} />}
-      </main>
-    </contextAuth.Provider>
+    <div>
+      <h2>Let's get started!</h2>
+    </div>
   );
 }
 
--- src/Context/auth-contrext.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const contextAuth = React.createContext({
-    isLoggedIn : false
-});
-
-export default contextAuth;
