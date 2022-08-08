6b6e72b3132f67c8830e8df4f8eb40cbfadd12ab
AndreasSitumorang <andreassitumorang098@gmail.com>
2022-08-08 10:31:33 +0700

    fix add existing feature

--- src/DataStore/ItemContextProvider.js
+++ src/DataStore/ItemContextProvider.js
@@ -2,46 +2,70 @@ import React, { useReducer } from "react";
 import ItemContext from "./item-context";
 
 const defaultItemCartState = {
-    items : [],
-    totalAmount : 0,
+  items: [],
+  totalAmount: 0,
 };
 
-const itemReducer = (state, action) =>{
-    if(action.type === 'ADD_ANDREAS'){
-        const udpatedItem = state.items.concat(action.item);
-        const udpatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
-        return{
-            items : udpatedItem,
-            totalAmount : udpatedTotalAmount,
-        }
+const itemReducer = (state, action) => {
+  if (action.type === "ADD_ANDREAS") {
+    const existingItemIndex = state.items.findIndex(
+      (item) => item.id === action.item.id
+    );
+    const existingCartItem = state.items[existingItemIndex];
+    // let updateItem;
+    let updateItems;
+    console.log(action.item);
+    if (existingCartItem) {
+      const updateItem = {
+        ...existingCartItem,
+        amount: existingCartItem.amount + action.item.amount,
+      };
+      updateItems = [...state.items];
+      updateItems[existingItemIndex] = updateItem;
+      console.log(updateItems[existingCartItem]);
+    } else {
+      updateItems = state.items.concat(action.item);
     }
-    return defaultItemCartState;
+    console.log(updateItems);
+    // const udpatedItem = state.items.concat(action.item);
+    const udpatedTotalAmount =
+      state.totalAmount + action.item.price * action.item.amount;
+    return {
+      items: updateItems,
+      totalAmount: udpatedTotalAmount,
+    };
+  }
+  return defaultItemCartState;
 };
 
-const ItemProvider = (props) =>{
-    const [itemState, dispatchItemAction] = useReducer(itemReducer, defaultItemCartState);
+const ItemProvider = (props) => {
+  const [itemState, dispatchItemAction] = useReducer(
+    itemReducer,
+    defaultItemCartState
+  );
 
-    const AddItemHandler = (item) =>{
-        dispatchItemAction({type : 'ADD_ANDREAS', item : item});
-    };
-    
-    const RemoveItemHandler = (id) =>{
-        dispatchItemAction({type : 'REMOVE_ANDREAS', id : id});
-    }
+  const AddItemHandler = (item) => {
+    dispatchItemAction({ type: "ADD_ANDREAS", item: item });
+  };
 
-    const ItemContextNeed = {
-        items : itemState.items,
-        totalAmount : itemState.totalAmount,
-        AddItem : AddItemHandler,
-        RemoveItem : RemoveItemHandler,
-    };
+  const RemoveItemHandler = (id) => {
+    dispatchItemAction({ type: "REMOVE_ANDREAS", id: id });
+  };
 
-    return(
+  const ItemContextNeed = {
+    items: itemState.items,
+    totalAmount: itemState.totalAmount,
+    AddItem: AddItemHandler,
+    RemoveItem: RemoveItemHandler,
+  };
+
+  return (
     <div>
-        <ItemContext.Provider value={ItemContextNeed}>
-            {props.children}
-        </ItemContext.Provider>
-    </div>);
-}; 
+      <ItemContext.Provider value={ItemContextNeed}>
+        {props.children}
+      </ItemContext.Provider>
+    </div>
+  );
+};
 
 export default ItemProvider;
